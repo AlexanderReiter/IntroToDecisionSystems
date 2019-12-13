f38fa333dcd5c288c8f45022ba8a58e74261af33
Alexander Reiter <alexander.reiter@htlvb.at>
2019-12-13 11:20:21 +0100

    Pairwise

--- TSP/Utils.cs
+++ TSP/Utils.cs
@@ -7,6 +7,11 @@ namespace TSPSolver.TSP
 {
     public static class Utils
     {
+        public static double CityDistance(Location city1, Location city2)
+        {
+            return Math.Sqrt(Math.Pow(city2.X - city1.X, 2) + Math.Pow(city2.Y - city1.Y, 2));
+        }
+
         public static double GetDistance(IReadOnlyList<int> solution, IReadOnlyList<Location> cities)
         {
             return solution
@@ -17,10 +22,6 @@ namespace TSPSolver.TSP
 
             //double distance = 0;
 
-            double CityDistance(Location city1, Location city2)
-            {
-                return Math.Sqrt(Math.Pow(city2.X - city1.X, 2) + Math.Pow(city2.Y - city1.Y, 2));
-            }
 
             //for(int i = 0; i < solution.Count - 1; i++)
             //{
@@ -30,6 +31,24 @@ namespace TSPSolver.TSP
             //distance += CityDistance(cities[solution[solution.Count - 1] - 1], cities[solution[0] - 1]);
 
             //return distance;
+        }
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<double> Pairwise(this IEnumerable<Location> source, Func<Location, Location, double> fn)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (fn is null)
+            {
+                throw new ArgumentNullException(nameof(fn));
+            }
+
+            return source.Zip(source.Skip(1), fn);
         }
     }
 }
