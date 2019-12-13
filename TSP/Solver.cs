24c8ab6e2255160b637b837184674dad7971d949
Alexander Reiter <alexander.reiter@htlvb.at>
2019-12-13 10:45:01 +0100

    abstract class Solver, Remake Distance

---
+++ TSP/Solver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSPSolver.TSP
+{
+    public abstract class Solver
+    {
+        public abstract IReadOnlyList<int> Solve(IReadOnlyList<Location> cities);
+    }
+
+    public class BruteForce : Solver
+    {
+        public override IReadOnlyList<int> Solve(IReadOnlyList<Location> cities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TSP/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSPSolver.TSP
+{
+    public static class Utils
+    {
+        public static double GetDistance(IReadOnlyList<int> solution, IReadOnlyList<Location> cities)
+        {
+            return solution
+                .Concat(solution.Take(1))
+                .Select(index => cities[index - 1])
+                .Pairwise(CityDistance)
+                .Sum();
+
+            //double distance = 0;
+
+            double CityDistance(Location city1, Location city2)
+            {
+                return Math.Sqrt(Math.Pow(city2.X - city1.X, 2) + Math.Pow(city2.Y - city1.Y, 2));
+            }
+
+            //for(int i = 0; i < solution.Count - 1; i++)
+            //{
+            //    distance += CityDistance(cities[solution[i] - 1], cities[solution[i + 1] - 1]);
+            //}
+
+            //distance += CityDistance(cities[solution[solution.Count - 1] - 1], cities[solution[0] - 1]);
+
+            //return distance;
+        }
+    }
+}
